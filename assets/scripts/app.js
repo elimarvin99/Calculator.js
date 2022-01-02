bb580346b798848b5522d43083c4d755885f5548
elimarvin99 <elimarvin99@gmail.com>
2022-01-01 21:07:26 -0500

    refactoring with if statements

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -20,49 +20,59 @@ function writeToLog (operationType, prevResult, operationNumber, newResult)
         operation: operationType,
         prevResult: prevResult,
         enteredNumber: operationNumber,
-        result: newResult 
+        newResult: newResult 
     };
     //push this new object to our array
     logEntries.push(logEntry);
     console.log(logEntries);
 }
+function calculateResult(calculationType)
+{
+    if (calculationType !== 'ADD' &&
+    calculationType !== 'SUBTRACT' &&
+    calculationType !== 'MULTIPLY' &&
+    calculationType !== 'DIVIDE') {
+        return;
+    }
+    const enteredNumber = getUserInput();
+    const initialResult = currentResult;
+    let mathOperator;
+    if (calculationType === 'ADD') {
+        currentResult += enteredNumber;
+        mathOperator = '+'
+    } else if (calculationType === 'SUBTRACT') {
+        currentResult -= enteredNumber;
+        mathOperator = '-'
+    } else if (calculationType === 'MULTIPLY') {
+        currentResult *= enteredNumber;
+        mathOperator = '*'
+    } else if (calculationType === 'DIVIDE') {
+        currentResult /= enteredNumber;
+        mathOperator = '/'
+    }
+    
+    createAndWriteOutput(mathOperator, initialResult, enteredNumber)
+    writeToLog(calculationType, initialResult, enteredNumber, currentResult);
+}
 
 function add()
 {
-    const enteredNumber = getUserInput();
-    const initialResult = currentResult; //this way we store the previous calc b4 it gets changed
-    //+ + also converts string to number
-    currentResult += enteredNumber //using global values as parameters so function affects the whole code that we don't have to pass in parameters
-    createAndWriteOutput('+', initialResult, enteredNumber)
-    //part of our function, that way it is displayed each time we make a calculation ("eventlistener")
-    writeToLog('ADD', initialResult, enteredNumber, currentResult);
+    calculateResult('ADD');
 }
 
 function subtract()
 {
-    const enteredNumber = getUserInput();
-    const initialResult = currentResult;
-    currentResult -= enteredNumber; 
-    createAndWriteOutput('-', initialResult, enteredNumber)
-    writeToLog('SUBTRACT', initialResult, enteredNumber, currentResult);
+    calculateResult('SUBTRACT');
 }
 
 function multiply()
 {
-    const enteredNumber = getUserInput();
-    const initialResult = currentResult;
-    currentResult *= enteredNumber; 
-    createAndWriteOutput('*', initialResult, enteredNumber)
-    writeToLog('MULTIPLY', initialResult, enteredNumber, currentResult);
+    calculateResult('MULTIPLY')
 }
 
 function divide()
 {
-    const enteredNumber = getUserInput();
-    const initialResult = currentResult;
-    currentResult /= enteredNumber; 
-    createAndWriteOutput('/', initialResult, enteredNumber)
-    writeToLog('DIVIDE', initialResult, enteredNumber, currentResult);
+    calculateResult('DIVIDE')
 }
 
 addBtn.addEventListener('click', add); //we only pass in the function name, that way it's called only when clicked. address to function. 
